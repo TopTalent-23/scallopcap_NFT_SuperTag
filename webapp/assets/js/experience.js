ecff5a44c11c42090b572b4dc9c788b50ae7d4bd
TopTalent-23 <mathieusleger4@gmail.com>
2023-12-09 21:15:23 -0500

    url change

--- webapp/assets/js/experience.js
+++ webapp/assets/js/experience.js
@@ -183,7 +183,7 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch("https://c83d-14-195-9-98.ngrok-free.app/api/generate", requestOptions)
+fetch("https://00e6-14-195-9-98.ngrok-free.app/api/generate", requestOptions)
   .then(response => response.text())
   .then(result => document.getElementById("result").innerHTML = JSON.parse(result).response)
   .catch(error => console.log('error', error));
